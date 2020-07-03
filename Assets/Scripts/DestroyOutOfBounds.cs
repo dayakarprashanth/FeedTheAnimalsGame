161d6119d42a1daa26d3713333c88b219a93ea05
dayakarprashanth <56269912+dayakarprashanth@users.noreply.github.com>
2020-07-03 12:47:24 -0500

    Comminting Food Fight Code

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBoundary = 24.0f ;
+    public float lowerBoundry = -12.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBoundary)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBoundry)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
